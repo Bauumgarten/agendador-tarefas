b823f2799101d891b056c186856d79b96a857865
Raul Baumgarten <raulbaumgarten@hotmail.com>
2025-12-03 09:00:56 -0300

    Adicionadas as configurações de segurança

---
+++ src/main/java/com/raul/agendador_tarefas/business/dto/UsuarioDTO.java
@@ -0,0 +1,15 @@
+package com.raul.agendador_tarefas.business.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UsuarioDTO {
+
+    private String email;
+    private String senha;
+
+}
